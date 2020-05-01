2baa0c7b8e3fe85f600db39e0b6f04a9b2598471
Chocohead <Chocohead@Aroma1997.org>
2020-05-01 15:41:51 +0100

    Hopefully take out the last of the LVT NPEs
    ASM does strange things here

--- src/main/java/net/fabricmc/loom/task/lvt/Verifier.java
+++ src/main/java/net/fabricmc/loom/task/lvt/Verifier.java
@@ -27,6 +27,7 @@ package net.fabricmc.loom.task.lvt;
 import java.util.List;
 
 import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.analysis.BasicValue;
 import org.objectweb.asm.tree.analysis.SimpleVerifier;
 
 import net.fabricmc.loom.task.lvt.ClassInfo.TypeLookup;
@@ -44,6 +45,40 @@ class Verifier extends SimpleVerifier {
         this.currentClassInterfaces = currentClassInterfaces;
         this.isInterface = isInterface;
     }
+
+	@Override
+	protected boolean isSubTypeOf(BasicValue value, BasicValue expected) {
+		Type expectedType = expected.getType();
+		Type type = value.getType();
+
+		switch (expectedType.getSort()) {
+			case Type.INT:
+			case Type.FLOAT:
+			case Type.LONG:
+			case Type.DOUBLE:
+				return type.equals(expectedType);
+
+			case Type.ARRAY:
+			case Type.OBJECT:
+				if (type.equals(NULL_TYPE)) {
+					return true;
+				} else if (type.getSort() == Type.OBJECT || type.getSort() == Type.ARRAY) {
+					if (isAssignableFrom(expectedType, type)) {
+						return true;
+					} else {
+						ClassInfo expectedTypeInfo = ClassInfo.forType(expectedType, TypeLookup.ELEMENT_TYPE);
+						//SimpleVerifier returns effectively this, somewhat questionable to whether it's true
+						//Suggests that so long as the expectedType is an interface it can be reached via type
+						//Even though #isAssignableFrom says that type doesn't have expectedType as a parent
+						return expectedTypeInfo != null && expectedTypeInfo.isInterface();
+					}
+				} else {
+					return false;
+				}
+			default:
+				throw new AssertionError();
+		}
+	}
 
     @Override
     protected boolean isInterface(final Type type) {
@@ -123,5 +158,10 @@ class Verifier extends SimpleVerifier {
 	        default:
 	        	throw new IllegalArgumentException("Unexpected type to try cast as " + type + ": " + other);
         }
+    }
+
+    @Override
+    protected Class<?> getClass(Type type) {
+    	throw new UnsupportedOperationException("Tried to load class: " + type);
     }
 }
