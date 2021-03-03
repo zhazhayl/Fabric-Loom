bbc86054f590ba2dd74d3c4c3dd754224917e559
Chocohead <Chocohead@Aroma1997.org>
2021-03-03 22:41:09 +0000

    Simplify refmap generation
    More likely to catch dodgy JSON this way

--- src/main/java/net/fabricmc/loom/util/MixinRefmapHelper.java
+++ src/main/java/net/fabricmc/loom/util/MixinRefmapHelper.java
@@ -35,6 +35,10 @@ import java.util.zip.ZipEntry;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
 import org.zeroturnaround.zip.ZipUtil;
 import org.zeroturnaround.zip.transform.StringZipEntryTransformer;
 import org.zeroturnaround.zip.transform.ZipEntryTransformerEntry;
@@ -46,23 +50,29 @@ public final class MixinRefmapHelper {
 
 	public static boolean addRefmapName(String filename, String mixinVersion, Path outputPath) {
 		File output = outputPath.toFile();
-		Set<String> mixinFilenames = findMixins(output, true);
+		Set<String> mixinFilenames = findMixins(output);
 
 		if (mixinFilenames.size() > 0) {
 			return ZipUtil.transformEntries(output, mixinFilenames.stream().map((f) -> new ZipEntryTransformerEntry(f, new StringZipEntryTransformer("UTF-8") {
 				@Override
 				protected String transform(ZipEntry zipEntry, String input) throws IOException {
-					JsonObject json = GSON.fromJson(input, JsonObject.class);
+					try {
+						JsonObject json = GSON.fromJson(input, JsonObject.class);
 
-					if (!json.has("refmap")) {
-						json.addProperty("refmap", filename);
+						if (!json.has("refmap")) {
+							json.addProperty("refmap", filename);
+						}
+
+						if (!json.has("minVersion") && mixinVersion != null) {
+							json.addProperty("minVersion", mixinVersion);
+						}
+
+						return GSON.toJson(json);
+					} catch (JsonSyntaxException e) {
+						System.err.println("Suspected Mixin config " + zipEntry + " is not a JSON object");
+						e.printStackTrace();
+						return input;
 					}
-
-					if (!json.has("minVersion") && mixinVersion != null) {
-						json.addProperty("minVersion", mixinVersion);
-					}
-
-					return GSON.toJson(json);
 				}
 			})).toArray(ZipEntryTransformerEntry[]::new));
 		} else {
@@ -70,29 +80,68 @@ public final class MixinRefmapHelper {
 		}
 	}
 
-	private static Set<String> findMixins(File output, boolean onlyWithoutRefmap) {
+	private static Set<String> findMixins(File output) {
 		// first, identify all of the mixin files
 		Set<String> mixinFilename = new HashSet<>();
 		// TODO: this is a lovely hack
 		ZipUtil.iterate(output, (stream, entry) -> {
 			if (!entry.isDirectory() && entry.getName().endsWith(".json") && !entry.getName().contains("/") && !entry.getName().contains("\\")) {
 				// JSON file in root directory
-				try (InputStreamReader inputStreamReader = new InputStreamReader(stream)) {
-					JsonObject json = GSON.fromJson(inputStreamReader, JsonObject.class);
+				try (JsonReader in = new JsonReader(new InputStreamReader(stream))) {
+					out: if (in.peek() == JsonToken.BEGIN_OBJECT) {
+						boolean hasPackage = false;
+						boolean probablyMixin = false;
+						boolean hasRefmap = false;
+						boolean hasMinVersion = false;
 
-					if (json != null) {
-						boolean hasMixins = json.has("mixins") && json.get("mixins").isJsonArray();
-						boolean hasClient = json.has("client") && json.get("client").isJsonArray();
-						boolean hasServer = json.has("server") && json.get("server").isJsonArray();
+						in.beginObject();
+						while (in.hasNext()) {
+							switch (in.nextName()) {
+							case "package":
+								if (in.peek() == JsonToken.STRING) {
+									hasPackage = true;
+								} else {
+									break out; //Not Mixin
+								}
+								break;
 
-                        if (json.has("package") && (hasMixins || hasClient || hasServer)) {
-                            if (!onlyWithoutRefmap || !json.has("refmap") || !json.has("minVersion")) {
-                                mixinFilename.add(entry.getName());
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    // ...
+							case "mixins":
+							case "client":
+							case "server":
+								if (in.peek() == JsonToken.BEGIN_ARRAY) {
+									probablyMixin = true;
+								} else {
+									break out; //Not Mixin
+								}
+								break;
+
+							case "refmap":
+								if (in.peek() == JsonToken.STRING) {
+									hasRefmap = true;
+								} else {
+									break out; //Not Mixin
+								}
+								break;
+
+							case "minVersion":
+								if (in.peek() == JsonToken.STRING || in.peek() == JsonToken.NUMBER) {
+									hasMinVersion = true;
+								} else {
+									break out; //Not Mixin
+								}
+								break;
+							}
+
+							in.skipValue(); //Don't actually mind what the value is
+						}
+
+						if (hasPackage && probablyMixin && (!hasRefmap || !hasMinVersion)) {
+							mixinFilename.add(entry.getName());
+						}
+					}
+                } catch (IOException | IllegalStateException e) {
+                	System.err.println("Error reading " + entry);
+                    e.printStackTrace();
                 }
             }
         });
